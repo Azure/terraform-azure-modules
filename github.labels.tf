1406d8333e4d6e96439f53c31363811fd275e3be
Jared Holgate <jaredholgate@microsoft.com>
2025-03-07 15:21:59 +0000

    Sort the odd label

--- github.labels.tf
+++ github.labels.tf
@@ -4,13 +4,18 @@ data "http" "labels" {
 
 locals {
   label_list = csvdecode(data.http.labels.response_body)
+  labels = { for label in local.label_list : label.Name => {
+    name = label.Name
+    color = label.HEX
+    description = strcontains(label.Description, ":") ? split(":", label.Description )[1] : label.Description
+  } }
 }
 
 resource "github_issue_label" "this" {
-  for_each = { for value in local.label_list : value.Name => value }
+  for_each = local.labels
 
   repository = data.github_repository.this.name
-  name       = each.value.Name
-  color      = each.value.HEX
-  description = substr(each.value.Description, 0 , 100)
+  name       = each.value.name
+  color      = each.value.color
+  description = substr(each.value.description, 0 , 100)
 }
